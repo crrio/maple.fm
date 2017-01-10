2c057aa2d59efbf24f0d1b0d2c49898859f9daa3
Tyler Corsair <tyler@tylercorsair.com>
2017-01-09 20:29:25 -0800

    Additional Item Stats, Fixes, and Preloader

--- resources/views/statistics/item.blade.php
+++ resources/views/statistics/item.blade.php
@@ -4,13 +4,47 @@
 Statistics
 @endsection
 
+@section('full')
+<div class="preloader-wrapper big active center" style="margin:20px auto; display: block;">
+  <div class="spinner-layer spinner-blue">
+    <div class="circle-clipper left">
+      <div class="circle"></div>
+    </div>
+    <div class="gap-patch">
+      <div class="circle"></div>
+    </div>
+    <div class="circle-clipper right">
+      <div class="circle"></div>
+    </div>
+  </div>
+</div>
+@endsection
+
 @section('content')
 <input type="hidden" id="itemId" value="{{ $itemId }}">
 <span class="item-title"><span id="name"></span></span>
-<span class="extra"><span id="level"></span> <span id="subcategory" class="level"></span></span>
-<div class="item-box">
-<span id="description"></span>
-</div>
+<span class="extra">
+  <span id="level"></span> 
+  <span id="subcategory"></span>
+</span>
+<div class="item-box hide"><span id="description"></span></div>
+<span id="incSTR"></span>
+<span id="incDEX"></span>
+<span id="incINT"></span>
+<span id="incLUK"></span>
+<span id="incMHP"></span>
+<span id="incMMP"></span>
+<span id="incPAD"></span>
+<span id="incMAD"></span>
+<span id="incPDD"></span>
+<span id="incACC"></span>
+<span id="incEVA"></span>
+<span id="incCraft"></span>
+<span id="incSpeed"></span>
+<span id="incJump"></span>
+<span id="bdR"></span>
+<span id="imdR"></span>
+<span id="tuc"></span>
 <span id="data"></span>
 @endsection
 
@@ -78,14 +112,63 @@ Statistics
 
   $.getJSON("//maplestory.io/api/item/" + itemId, function(data) {
     item = data.Equip;
+    $('.preloader-wrapper').addClass('hide');
 
     if (item !== null) {
       if (item.reqLevel !== null && item.reqLevel !== 0) {
         $('#level').html('Level '+item.reqLevel+'');
       }
 
-      if (item.reqLevel !== null && item.reqLevel !== 0) {
-        $('#level').html('Level '+item.reqLevel+'');
+      if (item.incSTR !== null) {
+        $('#incSTR').html('STR: +'+item.incSTR+'<br/>');
+      }
+      if (item.incDEX !== null) {
+        $('#incDEX').html('DEX: +'+item.incDEX+'<br/>');
+      }
+      if (item.incINT !== null) {
+        $('#incINT').html('INT: +'+item.incINT+'<br/>');
+      }
+      if (item.incLUK !== null) {
+        $('#incLUK').html('LUK: +'+item.incLUK+'<br/>');
+      }
+      if (item.incMHP !== null) {
+        $('#incMHP').html('HP: +'+item.incMHP+'<br/>');
+      }
+      if (item.incMMP !== null) {
+        $('#incMMP').html('HP: +'+item.incMMP+'<br/>');
+      }
+      if (item.incPAD !== null) {
+        $('#incPAD').html('Weapon Attack: +'+item.incPAD+'<br/>');
+      }
+      if (item.incMAD !== null) {
+        $('#incMAD').html('Magic Attack: +'+item.incMAD+'<br/>');
+      }
+      if (item.incPDD !== null) {
+        $('#incPDD').html('Defense: +'+item.incPDD+'<br/>');
+      }
+      if (item.incACC !== null) {
+        $('#incACC').html('Accuracy: +'+item.incACC+'<br/>');
+      }
+      if (item.incEVA !== null) {
+        $('#incEVA').html('Avoidability: +'+item.incEVA+'<br/>');
+      }
+      if (item.incCraft !== null) {
+        $('#incCraft').html('Diligence: +'+item.incCraft+'<br/>');
+      }
+      if (item.incSpeed !== null) {
+        $('#incSpeed').html('Speed: +'+item.incSpeed+'<br/>');
+      }
+      if (item.incJump !== null) {
+        $('#incJump').html('Jump: +'+item.incJump+'<br/>');
+      }
+      if (item.bdR !== null) {
+        $('#bdR').html('When attacking bosses, damage +'+item.bdR+'%<br/>');
+      }
+      if (item.imdR !== null) {
+        $('#imdR').html('Ignore Monster DEF: +'+item.imdR+'%<br/>');
+      }
+      if (item.tuc !== null) {
+        $('#tuc').html('Number of Upgrades Available: '+item.tuc+'<br/>');
       }
     }
 
@@ -95,7 +178,8 @@ Statistics
       $('#subcategory').html(data.SubCategory);
     }
 
-    if (data.description !== null) {
+    if (data.description !== "") {
+      $('.item-box').removeClass('hide');
       $('#description').html(formatDesc(data.description));
     }
 
@@ -103,7 +187,7 @@ Statistics
     console.log(data);
 
     // Item ID
-    $('.icon').attr('data-url','https://maplestory.io/api/item/'+ itemId +'/iconRaw');
+    $('.icon').attr('data-url', 'data:image/png;base64,'+data.Icon.IconRaw);
 
       var pixelPerfect = new CodePeg.PixelPerfect(
           $('.icon'), {
@@ -129,7 +213,7 @@ Statistics
   .extra {
     font-size: 1.4rem;
     font-weight: 400;
-    margin: 0px;
+    margin: 0px 0px 10px 0px;
     color: #4e89a4;
     display: block;
   }
