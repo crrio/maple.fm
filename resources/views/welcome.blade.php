61f9ab5bb311885c6f798f7e5f8c5c02dff1801a
Tyler Corsair <tyler@tylercorsair.com>
2017-03-18 18:57:36 -0700

    News jQuery -> crrio.news.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -57,23 +57,7 @@ Explore
 
 @section('js')
 <script src="/js/jquery.rss.js"></script>
-<script>
-$("#news").rss(
-  "http://blog.crr.io/rss/",
-  {
-    limit: 1,
-    offsetStart: false,
-    offsetEnd: false,
-    ssl: false,
-    host: 'feedrapp.info',
-    layoutTemplate: "<div class='feed-container'>{entries}</div>",
-    entryTemplate: '<h4 style="padding:5px 10px;background:#F1F1F1;"><a href="{url}">{title}</a></h4><p>{body}</p>',
-    dateFormat: 'MMMM Do, YYYY',
-    dateLocale: 'en',
-    // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
-    effect: 'slideFastSynced',
-  });
-</script>
 <script src="/js/typeahead.min.js"></script>
 <script src="/js/crrio.itemFunc.js"></script>
+<script src="/js/crrio.news.js"></script>
 @endsection
