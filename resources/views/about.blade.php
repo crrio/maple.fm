69d6619223a4cde42f78d39aa190ab3ac9000340
Tyler Corsair <tyler@tylercorsair.com>
2017-12-05 03:48:35 -0800

    Slight About updates

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,12 +9,13 @@ About
 @endsection
 
 @section('content')
+<h2>We're redefining the <b>MapleStory</b> market for everyone.</h2>
 <p class="flow-text">Built with the MapleStory community in mind, we offer each player the chance to succeed that they deserve. Our service provides innovative and useful tools for maplers to enjoy what it truely means to be a mapler!</p>
-<p class="flow-text">Founded in 2013, our service has been the cornerstone of the MapleStory economy for statistics, histoical pricing, and much more. We've helped impact over <b>1.5 million maplers</b>' experience playing MapleStory, and continue to do so for free in the spirit of free-to-play gaming.</p>
+<p class="flow-text">Founded in 2013, our service has been the cornerstone of the MapleStory economy for statistics, histoical pricing, and much more. We've helped positively impact over <b>1.5 million maplers</b>' experience playing MapleStory, and continue to do so for free in the spirit of free-to-play gaming.</p>
 <h4>Licenses & Disclaimers</h4>
 <p>All assets, resources, item names, icons, and descriptions regarding MapleStory thereof are the sole property of <a href="http://nexon.net">NEXON</a>.</p>
 <p>All market, character, and item information is provided by <a href="http://maplestory.io">MapleStory.io</a>, an unofficial platform for developers.</p>
-<p> We are <i>not an official MapleStory server nor fansite</i>; we merely provide this service for free.</p>
+<p> We are <i>not an official MapleStory server nor fansite</i>; we merely provide this service for free and <a href="//github.com/crrio/maple.fm/commit/{{ exec('git rev-parse --short HEAD') }}" target="_blank">open source</a>.</p>
 @endsection
 
 @section('sidebar')
